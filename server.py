6e9c37734b8ba5c37b07deab480e810df523dea1
Christian <chnicoloso@gmail.com>
2018-10-22 12:49:04 -0700

    add explicit app object to bottle.run

--- server.py
+++ server.py
@@ -79,9 +79,9 @@ def index():
 
 
 if __name__ == "__main__":
-    if (os.environ.get('APP_LOCATION') == 'heroku'):
-        run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    else:
-        run(host='localhost', port=8080, debug=True)
-
     _initialize()
+
+    if (os.environ.get('APP_LOCATION') == 'heroku'):
+        run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    else:
+        run(app, host='localhost', port=8080, debug=True)
